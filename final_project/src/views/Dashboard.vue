f660fcc3b470b9401fa1b3ece233591075e36ccd
darren229 <104969073+darren229@users.noreply.github.com>
2022-07-07 17:33:25 +0800

    Add files via upload

---
+++ final_project/src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  Dashboard
+</template>
+
+<script>
+export default {
+  created() {
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)renToken\s*\s*([^;]*).*$)|^.*$/, '$1')
+    console.log(token)
+    this.$http.defaults.headers.common.Authorization = token
+    const api = `${process.env.VUE_APP_API}api/user/check`
+    this.$http.post(api, this.user).then((res) => {
+      console.log(res)
+    })
+  }
+}
+</script>
